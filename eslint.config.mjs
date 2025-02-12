dc48e693b4d61b0ee4d3578f5dd34a37f4da3ce7
Cynthia Teeters <cynthiateeters@gmail.com>
2025-02-12 14:50:10 -0500

    chore: update project settings & ESLint cfg to align w/ assignment reqs
    - Adjusted configuration files (.editorconfig ) for consistency
    
    - Revised VSCode settings in .vscode/extensions.json and .vscode/settings.json
    
    - Updated ESLint configuration (eslint.config.mjs) to match assignment guidelines
    
    - Modified package.json metadata and scripts accordingly

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      "no-console": "off", // Allow console statements
+    },
+  },
   pluginJs.configs.recommended,
 ];
